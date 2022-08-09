f49d07d38bcea7b1edcd586d10edf1e15dddd5ba
fajaralf <202310072@student.ibik.ac.id>
2022-08-09 18:41:31 +0700

    portofolio

--- src/js/main.js
+++ src/js/main.js
@@ -29,3 +29,12 @@ function navcolor() {
 }
 
 window.addEventListener("scroll", navcolor);
+
+const elementclose = document.getElementsByClassName("overlay");
+
+for (let i = 0; i < elementclose.length; i++) {
+  elementclose[i].addEventListener("click", function () {
+    let url = window.location.href.substr(0, window.location.href.indexOf("#"));
+    window.location = url;
+  });
+}
